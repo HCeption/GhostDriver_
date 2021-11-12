bae338f90cc5d85a5ba9712e1fcb8e0214620eca
HCeption <15846@student.eadania.dk>
2021-11-12 11:55:14 +0100

    j

--- GameWorld.cs
+++ GameWorld.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
+using Microsoft.Xna.Framework.Audio;
 
 namespace GhostDriver_
 {
@@ -33,7 +34,7 @@ namespace GhostDriver_
         public static int spawnAmount;
         public static int addSpawnAmount;
 
-        
+        private SoundEffectInstance vroom;
 
 
         public GameWorld()
