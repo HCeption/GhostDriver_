1ab708f8a7b0aec96b1eb789cc4a8cc90841bba2
Roy42069360 <15274@student.eadania.dk>
2021-11-12 12:39:54 +0100

    n

--- ExplosionEffect.cs
+++ ExplosionEffect.cs
@@ -8,7 +8,7 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace GhostDriver_
 {
-    class ExplosionEffect : GameObject  //work hard
+    class ExplosionEffect : GameObject
     {
         private static Texture2D[] explosion = new Texture2D[8];
         private float explosionFPS = 0;
