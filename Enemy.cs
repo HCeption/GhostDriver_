a2b4872090c7781106fc1e00ce148a6f61bf79c7
Ihor Dovhun <15272@student.eadania.dk>
2021-11-15 10:40:15 +0100

    ads

--- Enemy.cs
+++ Enemy.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
+
 
 namespace GhostDriver_
 {
